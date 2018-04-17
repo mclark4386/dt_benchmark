44c26a3ee44357ec873f1dbd5d74bff4f55a6607
Matthew Clark <mclark4386@gmail.com>
2018-04-16 20:21:36 -0500

    *forgot files

---
+++ actions/navbar.go
@@ -0,0 +1,36 @@
+package actions
+
+import (
+	"cpsg-git.mattclark.guru/highlands/dt_benchmark/models"
+	"github.com/gobuffalo/buffalo"
+	"github.com/gobuffalo/pop"
+	"github.com/pkg/errors"
+)
+
+func SetupNavbar(next buffalo.Handler) buffalo.Handler {
+	return func(c buffalo.Context) error {
+		//Load Teams
+		teams := &models.Teams{}
+		tx, ok := c.Value("tx").(*pop.Connection)
+		if !ok {
+			return errors.WithStack(errors.New("no transaction found"))
+		}
+
+		err := tx.All(teams)
+		if err != nil {
+			return errors.WithStack(err)
+		}
+		c.Set("teams", teams)
+
+		//Load Campuses
+		campuses := &models.Teams{} // &models.Campuses{}
+
+		/*		err = tx.All(campuses)
+				if err != nil {
+					return errors.WithStack(err)
+				}
+		*/c.Set("campuses", campuses)
+
+		return next(c)
+	}
+}
