c33909e5a9f707dfd4cdf85db8c5128d82f9a723
John Lovejoy <johnlovejoy@Johns-MacBook-Pro.local>
2018-04-19 16:12:45 -0500

    Creaed the benchmark items. Commented out the tests. Added permissions for admins to edit items.

---
+++ actions/benchmark_items_test.go
@@ -0,0 +1,31 @@
+package actions
+
+/*
+func (as *ActionSuite) Test_BenchmarkItemsResource_List() {
+	as.Fail("Not Implemented!")
+}
+
+func (as *ActionSuite) Test_BenchmarkItemsResource_Show() {
+	as.Fail("Not Implemented!")
+}
+
+func (as *ActionSuite) Test_BenchmarkItemsResource_New() {
+	as.Fail("Not Implemented!")
+}
+
+func (as *ActionSuite) Test_BenchmarkItemsResource_Create() {
+	as.Fail("Not Implemented!")
+}
+
+func (as *ActionSuite) Test_BenchmarkItemsResource_Edit() {
+	as.Fail("Not Implemented!")
+}
+
+func (as *ActionSuite) Test_BenchmarkItemsResource_Update() {
+	as.Fail("Not Implemented!")
+}
+
+func (as *ActionSuite) Test_BenchmarkItemsResource_Destroy() {
+	as.Fail("Not Implemented!")
+}
+*/
